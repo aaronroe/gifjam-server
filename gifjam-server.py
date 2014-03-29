67add45c38456734966a60e38206468212b6c3ed
aaronroe <aaronalkan@gmail.com>
2014-03-29 14:10:49 -0500

    Bootstraps code for moviepy, pymongo

--- gifjam-server.py
+++ gifjam-server.py
@@ -1,6 +1,16 @@
-from flask import Flask
+import os
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+from moviepy.editor import *
+from flask.ext.pymongo import PyMongo
+
+UPLOAD_FOLDER = 'file-uploads'
+ALLOWED_EXTENSIONS = set(['mp4'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
